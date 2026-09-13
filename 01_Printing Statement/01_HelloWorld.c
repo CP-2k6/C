7693173f2631ffe4b518cf9d8c45d805aafbb8f1
CP-2k6 <cpraveen2006@outlook.com>
2026-09-13 15:43:39 +0530

    Add files via upload

---
+++ 01_Printing Statement/01_HelloWorld.c
@@ -0,0 +1,8 @@
+# include <stdio.h> // "stdio.h" is the pre-processor directive that contains the functions like printf & scanf. "# include" is the keyword to use "stdio.h". This line is called header file. 
+int main() // Starting of the main method
+{
+    printf("Hello World !"); // statement that will be executed
+    return 0; // marks the ending of the program
+} // ending of the main method
+
+// Compiler converts this code to machine code.
---
+++ 01_Printing Statement/02_Details.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main()
+{
+    printf("Name : C Praveen\n"); // "\n" is the escape sequence used fro new-line
+    printf("Age : 20 years\n");
+    printf("Adderss : Flat No. : 1721, Vijaya Heritage, Kadma, Jamshedpur - 831005\n");
+    return 0;
+}
